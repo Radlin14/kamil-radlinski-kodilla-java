a25c6aef79cdcec89ba9da80fa896907586f6087
Radlin14 <kamilrx8@gmail.com>
2023-12-29 09:42:57 +0100

    18.4

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -5,26 +5,31 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+
 @RestController
-@RequestMapping("/v1/tasks")
+@RequestMapping("v1/task")
 public class TaskController {
 
     @GetMapping
     public List<TaskDto> getTasks() {
         return new ArrayList<>();
     }
-    @GetMapping
-    public TaskDto getTask(Long taskId) {
+
+    @GetMapping("/{taskId}")
+    public TaskDto getTask( @PathVariable Long taskId) {
         return new TaskDto(1L, "test title", "test_content");
     }
+
     @DeleteMapping
     public void deleteTask(Long taskId) {
 
     }
+
     @PutMapping
     public TaskDto updateTask(TaskDto taskDto) {
         return new TaskDto(1L, "Edited test title", "Test content");
     }
+
     @PostMapping
     public void createTask(TaskDto taskDto) {
 
